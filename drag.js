5ae84a7541a71ce4ef21b796675fc695ca4b94c7
silencio9 <343096080@qq.com>
2016-12-08 19:01:14 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -8,8 +8,8 @@ function drag(id){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
 		document.onmousemove = function(ev){
-			obj.style.left = ev.pageX - disX + 'px';
-			obj.style.top = ev.pageY - disY + 'px';
+			//obj.style.left = ev.pageX - disX + 'px';
+			//obj.style.top = ev.pageY - disY + 'px';
 		};
 		document.onmouseup = function(){
 			document.onmousemove = null;
@@ -20,4 +20,7 @@ function drag(id){
 }
 
 function tab(id){
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
 }
